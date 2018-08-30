f83404383b255acbbebc1a542be299502df04d70
Jochem Berends <jochem.berends@ke-works.com>
2018-08-30 15:10:16 +0200

    sec5 lec67 css modules

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -180,9 +180,11 @@ module.exports = {
                     {
                       loader: require.resolve('css-loader'),
                       options: {
-                        importLoaders: 1,
                         minimize: true,
                         sourceMap: shouldUseSourceMap,
+                        importLoaders: 1,
+                        modules: true,
+                        localIdentName: '[name]__[local]__[hash:base64:5]'    
                       },
                     },
                     {
